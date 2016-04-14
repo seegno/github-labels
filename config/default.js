1c3139511b2780e51907d39cb4ab354b0904a397
Nuno Rafael Rocha <nunorafaelrocha@gmail.com>
2016-04-14 19:49:20 +0100

    Add configuration files

---
+++ config/default.js
@@ -0,0 +1,14 @@
+
+/**
+ * Export `default` configuration.
+ */
+
+module.exports = {
+  github: {
+    debug: false,
+    host: 'api.github.com',
+    protocol: 'https',
+    timeout: 5000,
+    version: '3.0.0'
+  }
+};
